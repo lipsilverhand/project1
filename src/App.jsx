025017771a0e177bb43925bf23a2c76e7495ed2a
lipsilverhand <williamhuynh99@gmail.com>
2024-02-03 20:34:37 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/header"
+import { Footer } from "./components/footer"
+import './App.css'
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <h1 className="active">Palworld</h1>
+            <p>ohowhefohwefhihefihweihfwehiob</p>
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import Logo from "../assets/moon.png"
+
+export const Header = () => {
+  return (
+    <div>
+        <img className="image" src={Logo} alt="" />
+    </div>
+  )
+}
